af60ed273963cf1f5561af349a3bcc266f000502
priscilla <priscillakung@gmail.com>
2023-05-15 14:20:51 -0700

    changed array to 2D array and created a checking answer function and allow user to do quiz over and over

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,9 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        "Four + Two is equal to Six",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"]
+        ["Four + Two is equal to Six", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
     
     var questionNumber = 0
     
@@ -30,12 +31,27 @@ class ViewController: UIViewController {
 
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        
+        let userAnswer = sender.currentTitle //True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong")
+        }
+        //or if questionNumber + 1 < quiz.count //count property returns the total number of elements in an array
+        if questionNumber < 2 { //this ensures the app doesn't crash if index goes out of range
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
         updateUI()
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
